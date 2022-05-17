e7d33224de099fa5fd7e74addb08761275da32e2
justiamkjh <cyberkjh89@gmail.com>
2022-05-17 23:42:07 +0900

    220517 baekjoon study

---
+++ src/baekjoon/A_Plus_B.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class A_Plus_B {
+
+	public static void main(String[] args) {
+		
+		int var1;
+		int var2;
+		int sum = 0;
+
+		Scanner sc = new Scanner(System.in);
+		do {
+			var1 = sc.nextInt();
+		}
+		while(var1<0);
+		
+		do {
+			var2 = sc.nextInt();
+		}
+		while(var2>10);
+		
+		sum = var1+var2;
+		
+		//System.out.println(var1+" + " + var2 +" = " + sum);
+		System.out.println(sum);
+	}
+
+}
