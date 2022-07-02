e75db6e74f400f6b7ed95f030e20292fe123d665
justiamkjh <cyberkjh89@gmail.com>
2022-07-02 23:49:28 +0900

    20220702 study

---
+++ src/baekjoon/Step6_String/CompareConst.java
@@ -0,0 +1,52 @@
+package baekjoon.Step6_String;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+
+public class CompareConst {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String input="";
+		String var1="";
+		String var2="";
+		
+		do {
+			input = sc.nextLine();
+		}while( input.contains("0") || input.length() > 3 );
+		
+		StringTokenizer st = new StringTokenizer(input, " ");
+		while(st.hasMoreTokens()) {
+			var1 = st.nextToken();
+			var2 = st.nextToken();
+		}
+		
+		do {
+			var2 = sc.next();
+		}while( var2.contains("0") || var2.length() > 3 || var2.equals(var1));
+		
+		String newVar1 = "";
+		String newVar2 = "";
+		
+		for(int i = var1.length()-1 ; i >= 0 ; i--) {
+			newVar1 = newVar1.concat(String.valueOf(var1.charAt(i))) ;
+		}
+		
+		for(int i = var2.length()-1 ; i >= 0 ; i--) {
+			newVar2 = newVar2.concat(String.valueOf(var2.charAt(i))) ;
+		}
+		
+		sc.close();
+		
+		int intvar1 = Integer.valueOf(newVar1);
+		int intvar2 = Integer.valueOf(newVar2);
+		
+		if(intvar1 > intvar2) {
+			System.out.println(intvar1);
+		}else {
+			System.out.println(intvar2);
+		}
+	}
+
+}
