166c12f5eb0e72748068b68a873f25eb3dbeef8c
justiamkjh <cyberkjh89@gmail.com>
2022-08-04 21:22:27 +0900

    220804 분수찾기 solving

---
+++ src/baekjoon/Step7_Mathematics/Find_Fountaion.java
@@ -0,0 +1,16 @@
+package baekjoon.Step7_Mathematics;
+
+import java.util.Scanner;
+
+public class Find_Fountaion {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int inputVal = sc.nextInt();
+		
+		
+	}
+
+}
